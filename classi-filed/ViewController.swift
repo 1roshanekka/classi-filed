6414a4d5cfe1b98f6931c9a7965b7a76ef7b59a6
Roshan Ekka <1ROSHANEKKA@GMAIL.COM>
2022-11-04 21:36:37 +0530

    Initial Commit

---
+++ classi-filed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  classi-filed
+//
+//  Created by Roshan Ekka on 04/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
